e61d9f82f58881595f75c7140f03a8d248edfa3a
Necromimic <keyzafalconx@hotmail.com>
2021-09-11 18:31:00 +0700

    imdone

--- web/views.py
+++ web/views.py
@@ -1,3 +1,4 @@
+from web.models import Medic, Patient
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
@@ -5,9 +6,13 @@ def index(request):
     return render(request, 'index.html')
 
 def patient(request):
+    context = {}
+    context ['patients'] = Patient.objects.all()
     return render(request, 'patient.html')
 
 def medic(request):
+    context = {}
+    context ['medics'] = Medic.objects.all()
     return render(request, 'medic.html')
 
 def hospital(request):
