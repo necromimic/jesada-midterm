fed8c99f756d29bae3d799f2d3220ce3ab53c0a3
Necromimic <keyzafalconx@hotmail.com>
2021-09-11 18:34:54 +0700

    imdone1

--- web/migrations/0020_hospital.py
+++ web/migrations/0020_hospital.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Hospital',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID', null=True)),
                 ('hosname', models.CharField(max_length=50, verbose_name='ชื่อโรงพยาบาล')),
                 ('patient', models.ForeignKey(on_delete=django.db.models.deletion.DO_NOTHING, to='web.patient')),
             ],
