7063e1c99869e90af8065038ec7f7c8d6ac75b89
Necromimic <keyzafalconx@hotmail.com>
2021-09-11 16:05:28 +0700

    Update8

--- web/admin.py
+++ web/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from web.models import Patient
+from web.models import Patient, Medic, Hospital
 
 # Register your models here.
 #admin.site.register(Patient)
 
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ['pfirst_name', 'plast_name', 'gender', 'age']
+    list_display = ['pfirst_name', 'plast_name', 'gender', 'age', 'medic']
+
+@admin.register(Medic)
+class MedicAdmin(admin.ModelAdmin):
+    list_display = ['mfirst_name', 'mlast_name', 'expertise']
+    
+
+@admin.register(Hospital)
+class HospitalAdmin(admin.ModelAdmin):
+    list_display = ['hosname', ]
+    
+    
+
     
--- web/models.py
+++ web/models.py
@@ -1,20 +1,58 @@
 from django.db import models
+from django.db.models.deletion import CASCADE, DO_NOTHING
 
-GENDER_CHOICES = [
-    (0, 'เลือกเพศ'),
+GENDER_CHOICES = [   
     (1, 'เพศชาย'),
     (2, 'เพศหญิง'),
     (3, 'เพศทางเลือก'),
 ]
 # Create your models here.
+class Hospital(models.Model):
+    """Model definition for Hospital."""
+
+    # TODO: Define fields here
+    hosname = models.CharField(("ชื่อโรงพยาบาล"), max_length=50)
+
+    class Meta:
+        """Meta definition for Hospital."""
+
+        verbose_name = 'Hospital'
+        verbose_name_plural = 'Hospitals'
+
+    def __str__(self):
+        """Unicode representation of Hospital."""
+        return self.hosname    
+
+class Medic(models.Model):
+    """Model definition for Medic."""
+
+    # TODO: Define fields here
+    mfirst_name = models.CharField(("ชื่อจริง"), max_length=255)
+    mlast_name = models.CharField(("นามสกุล"), max_length=255)
+    expertise = models.CharField(("ความเชี่ยวชาญ"), max_length=255)
+    affiliation = models.ManyToManyField(Hospital, default=None, null=True, blank=True)
+    
+    
+
+    class Meta:
+        """Meta definition for Medic."""
+
+        verbose_name = 'Medic'
+        verbose_name_plural = 'Medics'
+
+    def __str__(self):
+        """Unicode representation of Medic."""
+        return self.mfirst_name + " " + self.mlast_name + " " + self.expertis
 class Patient(models.Model):
     """Model definition for Patient."""
 
     # TODO: Define fields here
     pfirst_name = models.CharField(("ชื่อจริง"),max_length=255)
     plast_name = models.CharField(("นามสกุล"),max_length=255)
-    gender = models.IntegerField(("เพศ"),choices=GENDER_CHOICES, default=0)
+    gender = models.IntegerField(("เพศ"),choices=GENDER_CHOICES)
     age = models.CharField(("อายุ"), max_length=3, default='')
+    medic = models.OneToOneField(Medic, on_delete=models.DO_NOTHING, default='')
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE, default=None)
 
     class Meta:
         """Meta definition for Patient."""
@@ -24,7 +62,34 @@ class Patient(models.Model):
 
     def __str__(self):
         """Unicode representation of Patient."""
-        return self.pfirst_name + " " + self.plast_name + " " + self.age
+        return self.pfirst_name + " " + self.plast_name 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
